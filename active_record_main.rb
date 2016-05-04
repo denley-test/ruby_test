e1b77c507b1f1fa9e235d3fe18cf48d58ced67ec
Denley Hsiao <denley@justtodo.com>
2016-05-04 17:01:44 +0800

    use input db name

--- active_record_main.rb
+++ active_record_main.rb
@@ -2,11 +2,16 @@
 
 require 'active_record'
 
+
 lambda {
+  puts "Put your db name(default is party):"
+  db_name = gets.chomp
+  db_name = 'party' if db_name.empty?
+
   table_names = []
   ActiveRecord::Base.class_eval do
     establish_connection :adapter => "sqlite3",
-                         :database => "thirdsource/jhgsxx/projectdata/partyserver.db"
+                         :database => "thirdsource/jhgsxx/projectdata/#{db_name}server.db"
     self.pluralize_table_names = false
     table_names = connection.data_sources
   end
@@ -14,6 +19,7 @@ lambda {
   table_names.each do |table_name|
     temp = Class.new(ActiveRecord::Base) do
       self.table_name = table_name
+      self.primary_key = :id
     end
     eval "#{table_name.gsub(/^(?:dst|tbl)/, '')} = temp"
   end
