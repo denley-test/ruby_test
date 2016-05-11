9b46f53a0ee60c0940f1938bf94c4f2e0fa19c5f
Denley Hsiao <denley@justtodo.com>
2016-05-11 22:03:34 +0800

    refact active_record_main

--- active_record_main.rb
+++ active_record_main.rb
@@ -1,19 +1,14 @@
 #! /usr/bin/env ruby
 
 require 'active_record'
+require_relative 'get_params'
 
-
-lambda {
-  # assign db_name
-  puts "Enter current db name(default is party):"
-  db_name = gets.chomp
-  db_name = 'party' if db_name.empty?
-
+def execute_db(db_file)
   # get table names from db_name
   table_names = []
   ActiveRecord::Base.class_eval do
     establish_connection :adapter => "sqlite3",
-                         :database => "thirdresource/jhgsxx/projectdata/#{db_name}server.db"
+                         :database => db_file
     self.pluralize_table_names = false
     table_names = connection.data_sources
   end
@@ -27,6 +22,12 @@ lambda {
     end
     model_name = table_name.sub(/^(?:dst|tbl)/, '').classify
     eval "#{model_name} = temp"
-    puts model_name
+    puts "  #{model_name}"
   end
+end
+
+lambda {
+  project_name, db_name = get_params(:project_name=>'jhgsxx', :db_name=>"PartyServer.db")
+  db_file = "thirdresource/#{project_name}/projectdata/#{db_name}"
+  execute_db(db_file)
 }.call
