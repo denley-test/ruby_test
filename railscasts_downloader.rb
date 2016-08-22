cdfe3b12ccbec53764e65dabb961f7d91dd722dd
Denley Hsiao <denley@justtodo.com>
2016-08-22 10:39:23 +0800

    FIX BUG: URI already defined.

--- railscasts_downloader.rb
+++ railscasts_downloader.rb
@@ -3,9 +3,8 @@
 require 'open-uri'
 require 'rss'
 
-URI = 'http://railscasts.com/episodes.rss?ext=mp4'
-
-open(URI) do |f|
+RAILS_CASTS_URI = 'http://railscasts.com/episodes.rss?ext=mp4'
+open(RAILS_CASTS_URI) do |f|
   result = RSS::Parser.parse(f.read, false)
   result.items.reverse!.each do |item|
     puts item.enclosure.url
